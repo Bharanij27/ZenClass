f045d801b77728d059bf877d1c67f9df8b50d5d1
BHARATH KUMAR <bhavanisandhiya58@gmail.com>
2020-08-27 00:35:41 +0530

    GUVI Week 6 Assignment

--- week6/Day-3/index.js
+++ week6/Day-3/index.js
@@ -1,11 +1,87 @@
+var Netflix = /** @class */ (function () {
+    function Netflix(videoURLs) {
+        var _this = this;
+        this.powerToggle = function () {
+            if (!_this.player.getAttribute("src")) {
+                _this.player.setAttribute("src", _this.songs[_this.currentIndex]);
+                _this.player.load();
+                _this.player.play();
+            }
+            else {
+                _this.player.setAttribute("src", "");
+            }
+        };
+        this.adjustVolume = function (value) {
+            0 < _this.player.volume &&
+                _this.player.volume < 1 &&
+                (_this.player.volume += value);
+        };
+        this.mutePlayer = function () {
+            _this.player.muted = !_this.player.muted;
+        };
+        this.pausePlayer = function () {
+            if (_this.player.getAttribute("src")) {
+                if (!_this.player.paused)
+                    _this.player.pause();
+                else
+                    _this.player.play();
+            }
+        };
+        var video = document.querySelector("video");
+        video.setAttribute("id", "my-video");
+        video.setAttribute("type", "video/mp4");
+        document.querySelector("#first-div").append(video);
+        this.songs = videoURLs;
+        this.currentIndex = 0;
+        this.player = video;
+        this.player.volume = 0.5;
+    }
+    Netflix.prototype.changeChannel = function (value) {
+        if (this.songs[this.currentIndex + value]) {
+            this.currentIndex += value;
+        }
+        else {
+            if (value == 1)
+                this.currentIndex = 0;
+            else
+                this.currentIndex = this.songs.length - 1;
+        }
+        this.player.setAttribute("src", this.songs[this.currentIndex]);
+        this.player.load();
+        this.player.play();
+    };
+    return Netflix;
+}());
 var videos = [
-    'https://www.youtube.com/embed/thtAxtEuX6c?controls=0',
-    'https://www.youtube-nocookie.com/embed/_-pdljs3KJA?controls=0',
-    'https://www.youtube.com/embed/OjU54VhRFbU?controls=0&amp;start=6',
-    'https://www.youtube.com/embed/FzLpP8VBC6E?controls=0',
-    'https://www.youtube.com/embed/OECd8L9wLpQ?controls=0'
+    "https://ak.picdn.net/shutterstock/videos/1040929034/preview/stock-footage-crab-working-in-seashore-full-focused-shot-of-crab-it-building-the-cave-it-making-holes-in-sea-sand.webm",
+    "https://ak.picdn.net/shutterstock/videos/30423748/preview/stock-footage-ilica-waterfall-video-is-located-in-ilica-village-near-pinarbasi-town-kastamonu-turkey.webm",
+    "https://ak.picdn.net/shutterstock/videos/1024163294/preview/stock-footage-circa-s-the-will-bradley-orchestra-performs-boardwalk-boogie-in-this-upbeat-soundie-from-the.webm",
 ];
-var craeteFrame = function () {
-    console.log('on');
+var tvObject = new Netflix(videos);
+var TV = function (operation) {
+    switch (operation) {
+        case "power":
+            tvObject.powerToggle();
+            break;
+        case "mute":
+            tvObject.mutePlayer();
+            break;
+        case "pause":
+            tvObject.pausePlayer();
+            break;
+        case "plusVolume":
+            tvObject.adjustVolume(+0.05);
+            break;
+        case "negVolume":
+            tvObject.adjustVolume(-0.05);
+            break;
+        case "plusChannel":
+            tvObject.changeChannel(+1);
+            break;
+        case "negChannel":
+            tvObject.changeChannel(-1);
+            break;
+        default:
+            break;
+    }
 };
-// <iframe id="my-video" src="https://www.youtube.com/embed/thtAxtEuX6c?controls=0" frameborder="0"></iframe>
--- week6/Day-3/index.ts
+++ week6/Day-3/index.ts
@@ -1,13 +1,94 @@
-let videos = [
-    'https://www.youtube.com/embed/thtAxtEuX6c?controls=0',
-    'https://www.youtube-nocookie.com/embed/_-pdljs3KJA?controls=0',
-    'https://www.youtube.com/embed/OjU54VhRFbU?controls=0&amp;start=6',
-    'https://www.youtube.com/embed/FzLpP8VBC6E?controls=0',
-    'https://www.youtube.com/embed/OECd8L9wLpQ?controls=0'
-]
+class Netflix {
+  songs: string[];
+  currentIndex: number;
+  player: HTMLVideoElement;
 
-let craeteFrame = () =>{
-    console.log('on')
+  constructor(videoURLs) {
+    let video = document.querySelector("video");
+    video.setAttribute("id", "my-video");
+    video.setAttribute("type", "video/mp4");
+    document.querySelector("#first-div").append(video);
+
+    this.songs = videoURLs;
+    this.currentIndex = 0;
+    this.player = video;
+    this.player.volume = 0.5;
+  }
+
+  powerToggle = () => {
+    if (!this.player.getAttribute("src")) {
+      this.player.setAttribute("src", this.songs[this.currentIndex]);
+      this.player.load();
+      this.player.play();
+    } else {
+      this.player.setAttribute("src", "");
+    }
+  };
+
+  adjustVolume = (value: number) => {
+    0 < this.player.volume &&
+      this.player.volume < 1 &&
+      (this.player.volume += value);
+  };
+
+  mutePlayer = () => {
+    this.player.muted = !this.player.muted;
+  };
+
+  pausePlayer = () => {
+    if (this.player.getAttribute("src")) {
+      if (!this.player.paused) this.player.pause();
+      else this.player.play();
+    }
+  };
+
+  changeChannel(value) {
+    if (this.songs[this.currentIndex + value]) {
+      this.currentIndex += value;
+    }
+    else{
+        if(value == 1) this.currentIndex = 0;
+        else this.currentIndex = this.songs.length - 1;
+    }
+    this.player.setAttribute("src", this.songs[this.currentIndex]);
+      this.player.load();
+      this.player.play();
+  }
 }
 
-// <iframe id="my-video" src="https://www.youtube.com/embed/thtAxtEuX6c?controls=0" frameborder="0"></iframe>
+let videos = [
+  "https://ak.picdn.net/shutterstock/videos/1040929034/preview/stock-footage-crab-working-in-seashore-full-focused-shot-of-crab-it-building-the-cave-it-making-holes-in-sea-sand.webm",
+  "https://ak.picdn.net/shutterstock/videos/30423748/preview/stock-footage-ilica-waterfall-video-is-located-in-ilica-village-near-pinarbasi-town-kastamonu-turkey.webm",
+  "https://ak.picdn.net/shutterstock/videos/1024163294/preview/stock-footage-circa-s-the-will-bradley-orchestra-performs-boardwalk-boogie-in-this-upbeat-soundie-from-the.webm",
+];
+
+let tvObject = new Netflix(videos);
+
+let TV = (operation) => {
+  switch (operation) {
+    case "power":
+      tvObject.powerToggle();
+      break;
+    case "mute":
+      tvObject.mutePlayer();
+      break;
+    case "pause":
+      tvObject.pausePlayer();
+      break;
+    case "plusVolume":
+      tvObject.adjustVolume(+0.05);
+      break;
+    case "negVolume":
+      tvObject.adjustVolume(-0.05);
+      break;
+    case "plusChannel":
+      tvObject.changeChannel(+1);
+      break;
+    case "negChannel":
+      tvObject.changeChannel(-1);
+      break;
+
+    default:
+      break;
+  }
+};
