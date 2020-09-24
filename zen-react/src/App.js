07ad6d76e0270b1c52960eb7f07c189f618c93c9
BHARATH KUMAR <bhavanisandhiya58@gmail.com>
2020-09-24 13:54:21 +0530

    React Basic

---
+++ zen-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyComponent from './MyComponent.js';
+
+function App() {
+  return (
+    <div className = "Name">
+    <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
